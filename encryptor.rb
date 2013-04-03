3b985807aa0862de9da279171a458cf3ac7f68cf
Fernando Paredes <nanoxd@me.com>
2013-04-02 21:34:31 -0700

    Add string encryptor

--- encryptor.rb
+++ encryptor.rb
@@ -10,8 +10,24 @@ class Encryptor
      'y' => 'l', 'z' => 'm'}
   end
   
-  def encrypt(letter)
+  def encrypt_letter(letter)
+    lowercase_letter = letter.downcase
     cipher[letter.downcase]
+  end
+  
+  def encrypt(string)
+    # Cuts the input string into letters
+    letters = string.split("")
+    
+    # Encrypts letters one at a time
+    results = []
+    letters.each do |letter|
+      encrypted_letter = encrypt_letter(letter)
+      results.push(encrypted_letter)
+    end
+    
+    # Join the results back together in one string
+    results.join
   end
   
 end
